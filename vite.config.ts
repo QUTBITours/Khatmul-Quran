fbac308893519ea470f84f76c24ae4f1d483cf5c
QUTBITours <qtholidays@gmail.com>
2025-05-10 17:32:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Khatmul-Quran/', // Set this to your repository name
+  base: '/Khatmul-Quran/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
